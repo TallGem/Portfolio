9edff6667fd657c36aa46cc0dc17196f9b13ea6a
operatall <muftauabdulafees18@gmail.com>
2024-05-21 16:57:20 +0100

    worked  more o the services section

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FaServicestack } from "react-icons/fa6";
 import { SiWebmoney } from "react-icons/si";
 import { PiWebhooksLogoFill } from "react-icons/pi";
+import { SiSemanticweb } from "react-icons/si";
 
 const Services = () => {
   return (
@@ -14,30 +15,47 @@ const Services = () => {
           <h1 className='text-2xl font-semibold'>⪼⪼⪼ Services:</h1>
 
           <div className='mt-20'>
-            <h3 className='text-xl font-semibold flex gap-2'> <span className='text-purple'><SiWebmoney /></span>Web Designing</h3>
-            
-            <p className='font-semibold text-purple-900'>
-            I believe in crafting web experiences that seamlessly blend form and function. By prioritizing clean aesthetics, intuitive navigation, and purposeful design elements, I strive to create websites that not only look beautiful but also enhance the user's journey.
+            <div className='flex items-center gap-1 '>
+
+              <h3 className='text-purple items-center text-xl '><SiWebmoney /></h3>
+              <h3 className='text-xl font-semibold flex'> Web Design</h3>
+
+            </div>
+
+            <p className='font-semibold text-purple-900 pt-2'>
+              I believe in crafting web experiences that seamlessly blend form and function. By prioritizing clean aesthetics, intuitive navigation, and purposeful design elements, I strive to create websites that not only look beautiful but also enhance the user's journey.
             </p>
-            
+
           </div>
 
           <div className='mt-10'>
-            <h3 className='text-xl font-semibold flex gap-2'> <span className='text-purple'><PiWebhooksLogoFill /></span>Progressive Web Application</h3>
-            
-            <p className='font-semibold text-purple-900'>
-            As a seasoned web developer, I specialize in crafting high-performance PWAs that push the boundaries of what's possible on the web.  I ensure that PWAs adapt gracefully to desktops, tablets, and smartphones alike. Whether accessed in portrait or landscape mode, users can expect a consistent and optimized experiece.
+
+            <div className=' flex items-center gap-1'>
+
+              <h3 className='text-purple text-2xl'><PiWebhooksLogoFill /></h3>
+              <h3 className='text-xl font-semibold flex gap-2'> Progressive Web Application</h3>
+
+            </div>
+
+            <p className='font-semibold text-purple-900 pt-2'>
+              As a seasoned web developer, I specialize in crafting high-performance PWAs that push the boundaries of what's possible on the web.  I ensure that PWAs adapt gracefully to desktops, tablets, and smartphones alike. Whether accessed in portrait or landscape mode, users can expect a consistent and optimized experiece.
             </p>
-            
+
           </div>
 
           <div className='mt-10'>
-            <h3 className='text-xl font-semibold flex gap-2'> <span className='text-purple'><FaServicestack /></span>UI/UX Design</h3>
-            
-            <p className='font-semibold text-purple-900'>
-            I specialize in creating visually appealing and functional user interfaces that elevate the overall user experience. From typography and color palettes to layout and visual hierarchy, I pay meticulous attention to every detail to ensure clarity, consistency, and aesthetic appeal.
+
+            <div className='flex items-center gap-1'>
+
+              <h3 className='text-purple text-xl'> <SiSemanticweb /></h3>
+              <h3 className='text-xl font-semibold'> UI/UX Design</h3>
+
+            </div>
+
+            <p className='font-semibold text-purple-900 pt-2'>
+              I specialize in creating visually appealing and functional user interfaces that elevate the overall user experience. From typography and color palettes to layout and visual hierarchy, I pay meticulous attention to every detail to ensure clarity, consistency, and aesthetic appeal.
             </p>
-            
+
           </div>
 
         </div>
