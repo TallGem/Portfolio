6a5f8f38a6481e5f6110cc562ac40ae37730a5fa
operatall <muftauabdulafees18@gmail.com>
2025-12-07 19:08:50 +0100

    add some touches to contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,19 +1,55 @@
 import React from 'react'
-
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 const Contact = () => {
   return (
 
-    <div className='grid mx-5 px-10' >
-      <div>
+    <div className='grid lg:grid-cols-2 mx-5 px-10 my-10' >
+      <div className='bg-blue-600'>
 
         <h3 className='text text-2xl font-semibold'>
           ⪼⪼⪼ Contact:
         </h3>
 
+
+
       </div>
 
-      <div>
-
+      <div className='bg-red-500' >
+        <div>
+          <h1>Any place in your app!</h1>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = 'Required';
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = 'Invalid email address';
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <Field type="email" name="email" />
+                <ErrorMessage name="email" component="div" />
+                <Field type="password" name="password" />
+                <ErrorMessage name="password" component="div" />
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </div>
 
     </div>
