eba41e712b9c4c08d8e983a11098d9834796c15e
operatall <muftauabdulafees18@gmail.com>
2026-01-03 22:30:40 +0100

    stored

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
 
       <div className='bg-purple-600' >
         <div>
-          <h1>Any place in your app it's going to work at a flash speed?</h1>
+          <h1>Any place in your app it's going to work at a flash speed:?</h1>
           <Formik
             initialValues={{ email: '', password: '' }}
             validate={values => {
@@ -39,13 +39,13 @@ const Contact = () => {
             }}
           >
             {({ isSubmitting }) => (
-              <Form className='text-blue-300 bg-orange-200'>
+              <Form className='text-blue-600 bg-orange-500'>
                 <Field type="email" name="email" />
                 <ErrorMessage name="email" component="div" />
                 <Field type="password" name="password" />
                 <ErrorMessage name="password" component="div" />
                 <button type="submit" disabled={isSubmitting} className='bg-red-500 outline-green-200' >
-                  Submit'testing 01-2078'
+                  Submit'testing 01-206'
                 </button>
               </Form>
             )}
